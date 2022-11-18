59f1ce93d46b692afd13767195ff72dc0630312a
LizzieDworsky <amydworsky15@gmail.com>
2022-11-18 14:49:26 -0700

    added Thumbs up and down conditional renders based on boolean isLiked or isDisliked fields from the posts

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,3 +1,4 @@
+import { FaThumbsDown, FaThumbsUp } from "react-icons/fa";
 import React from "react";
 
 const Post = ({ post }) => {
@@ -5,6 +6,17 @@ const Post = ({ post }) => {
         <div>
             <p>{post.name}</p>
             <p>{post.message}</p>
+            {post.isLiked ? (
+                <FaThumbsUp className="like-dislike-svg thumb-up" />
+            ) : (
+                <FaThumbsUp className="like-dislike-svg" />
+            )}
+            {post.isDisliked ? (
+                <FaThumbsDown className="like-dislike-svg thumb-down" />
+            ) : (
+                <FaThumbsDown className="like-dislike-svg" />
+            )}
+            {/* add conditional based on True/False change classname to reflect if clicked--liked/disliked */}
         </div>
     );
 };
